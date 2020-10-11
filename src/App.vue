1c44c55f60d79dceeb5f0af8aff567a9eb489876
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-11 19:37:25 -0300

    Estilizando e criando header e footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,84 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <TheHeader />
+    <main id="main">
+      <router-view />
+    </main>
+    <TheFooter />
   </div>
 </template>
 
 <script>
+import TheHeader from "@/components/TheHeader.vue";
+import TheFooter from "@/components/TheFooter.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    TheHeader,
+    TheFooter,
+  },
 };
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+body,
+ul,
+li,
+h1,
+h2,
+p {
+  padding: 0;
+  margin: 0;
+}
+
+ul {
+  list-style: none;
+}
+
+body {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  color: #345;
+  background: url("./assets/pattern.svg") repeat top;
+}
+
+a {
+  color: #345;
+  list-style: none;
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  display: block;
+}
+
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background: #87f;
+  color: #fff;
+  text-align: center;
+  font-size: 1rem;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+  transition: all 0.3s;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #65d;
+  transform: scale(1.1);
+}
+
+#app {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+main {
+  flex: 1;
+}
 </style>
