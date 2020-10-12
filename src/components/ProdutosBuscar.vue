5cce6a37b06a1a8bff5a3bc7400257f77083134c
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-11 22:50:44 -0300

    Estilizando a pesquisa.

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -4,7 +4,7 @@
     <input
       type="submit"
       id="lupa"
-      value="Bucas"
+      value="Buscar"
       @click.prevent="buscarProdutos"
     />
   </form>
@@ -27,4 +27,35 @@ export default {
 </script>
 
 <style scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative;
+}
+#busca {
+  width: 100%;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 20px;
+  border: none;
+  border-radius: 4px;
+  transition: all 0.3s;
+}
+#busca:focus,
+#busca:hover {
+  outline: none;
+  transform: scale(1.1);
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+}
+
+#lupa {
+  width: 62px;
+  height: 62px;
+  background: url("../assets/search.svg") no-repeat center center;
+  text-indent: -150px;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  right: 0;
+}
 </style>
