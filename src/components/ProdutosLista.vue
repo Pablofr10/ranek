97671ac1e3ef19533989f2b34f8d9a15fd1d1c0d
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-12 16:05:12 -0300

    Criando produto individual.

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -3,13 +3,13 @@
     <transition mode="out-in">
       <div v-if="produtos && produtos.length" class="produtos" key="produto">
         <div class="produto" v-for="(produto, index) in produtos" :key="index">
-          <router-link to="/">
+          <router-link :to="{ name: 'produto', params: { id: produto.id } }">
             <img
               v-if="produto.fotos"
               :src="produto.fotos[0].src"
               :alt="produto.fotos[0].alt"
             />
-            <p class="preco">{{ produto.preco }}</p>
+            <p class="preco">{{ produto.preco | numeroPreco }}</p>
             <h2 class="titulo">{{ produto.nome }}</h2>
             <p>{{ produto.descricao }}</p>
           </router-link>
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/views/Home.vue';
+import Produto from '@/views/Produto.vue';
 
 Vue.use(Router);
 
@@ -12,8 +13,14 @@ export default new Router({
       name: 'home',
       component: Home,
     },
+    {
+      path: '/produto/:id',
+      name: 'produto',
+      component: Produto,
+      props: true,
+    },
   ],
   scrollBehavior() {
-    return window.screenTop({ top: 0, behavior: 'smooth' });
+    return window.scrollTo({ top: 0, behavior: 'smooth' });
   },
 });
