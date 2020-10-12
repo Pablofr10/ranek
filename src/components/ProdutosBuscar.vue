1a9329c1fdc2ca01107018cf2b77878e3f357aad
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-11 21:54:29 -0300

    Adicionando query string a busca

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,10 +1,28 @@
 <template>
-  <div></div>
+  <form>
+    <input type="text" name="busca" id="busca" v-model="busca" />
+    <input
+      type="submit"
+      id="lupa"
+      value="Bucas"
+      @click.prevent="buscarProdutos"
+    />
+  </form>
 </template>
 
 <script>
 export default {
   name: "ProdutosBuscar",
+  data() {
+    return {
+      busca: "",
+    };
+  },
+  methods: {
+    buscarProdutos() {
+      this.$router.push({ query: { q: this.busca } });
+    },
+  },
 };
 </script>
 
