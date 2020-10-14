05c7c936a81e1a409240ec4c8d9f0bba81c2a513
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-14 00:29:18 -0300

    Adiconando tela de produtos.

--- src/store.js
+++ src/store.js
@@ -20,6 +20,7 @@ export default new Vuex.Store({
       cidade: '',
       estado: '',
     },
+    usuario_produtos: null,
   },
   mutations: {
     UPDATE_LOGIN(state, payload) {
@@ -28,8 +29,20 @@ export default new Vuex.Store({
     UPDATE_USUARIO(state, payload) {
       state.usuario = Object.assign(state.usuario, payload);
     },
+    UPDATE_USUARIO_PRODUTOS(state, payload) {
+      state.usuario_produtos = payload;
+    },
+    ADD_USUARIO_PRODUTOS(state, payload) {
+      state.usuario_produtos.unshit(payload);
+    },
   },
   actions: {
+    getUsuarioProdutos(context) {
+      api.get(`/produto?usuario_id=${context.state.usuario.id}`).then((res) => {
+        console.log(res);
+        context.commit('UPDATE_USUARIO_PRODUTOS', res.data);
+      });
+    },
     getUsuario(context, payload) {
       return api.get(`/usuario/${payload}`).then((res) => {
         context.commit('UPDATE_USUARIO', res.data);
--- src/views/usuario/Usuario.vue
+++ src/views/usuario/Usuario.vue
@@ -17,7 +17,7 @@
           >
         </li>
         <li>
-          <button class="btn" @click.prevent="deslogar">Deslogar</button>
+          <button @click.prevent="deslogar">Deslogar</button>
         </li>
       </ul>
     </nav>
@@ -39,9 +39,44 @@ export default {
 };
 </script>
 
-<style scoped>
-.sidenav ul {
-  display: flex;
-  gap: 20px;
+<style>
+.usuario {
+  display: grid;
+  grid-template-columns: minmax(140px, 200px) 1fr;
+  max-width: 900px;
+  margin: 40px auto;
+  gap: 30px;
+  padding: 20px;
+}
+
+@media screen and (max-width: 500px) {
+  .usuario {
+    grid-template-columns: 1fr;
+    margin: 0px auto;
+  }
+}
+
+.sidenav a,
+.sidenav button {
+  padding: 10px;
+  display: block;
+  background: #f4f7fc;
+  margin-bottom: 10px;
+  border-radius: 4px;
+}
+
+.sidenav a.router-link-exact-active,
+.sidenav a:hover,
+.sidenav button:hover {
+  background: #87f;
+  color: #fff;
+}
+.sidenav button {
+  border: none;
+  width: 100%;
+  font-size: 1rem;
+  text-align: left;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  cursor: pointer;
 }
 </style>
