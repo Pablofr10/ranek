3a7639f0a2ebf895a635607736e437ee29640d13
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-17 17:26:48 -0300

    Finalizando compra.

--- src/views/usuario/UsuarioCompras.vue
+++ src/views/usuario/UsuarioCompras.vue
@@ -1,12 +1,70 @@
 <template>
-  <div>
-    <h1>Lista de Produtos</h1>
-  </div>
+  <section>
+    <div v-if="compras">
+      <h2>Compras</h2>
+      <div
+        class="produtos-wrapper"
+        v-for="(compra, index) in compras"
+        :key="index"
+      >
+        <ProdutoItem v-if="compra.produto" :produto="compra.produto">
+          <p class="vendedor">
+            <span>Vendedor: </span>
+            {{ compra.vendedor_id }}
+          </p>
+        </ProdutoItem>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+import ProdutoItem from "@/components/ProdutoItem.vue";
+import { api } from "@/services.js";
+import { mapState } from "vuex";
+
+export default {
+  data() {
+    return {
+      compras: null,
+    };
+  },
+  components: {
+    ProdutoItem,
+  },
+  computed: {
+    ...mapState(["usuario", "login"]),
+  },
+  methods: {
+    getCompras() {
+      api.get(`/transacao?comprador_id=${this.usuario.id}`).then((res) => {
+        this.compras = res.data;
+      });
+    },
+  },
+  watch: {
+    login() {
+      this.getCompras();
+    },
+  },
+  created() {
+    if (this.login) {
+      this.getCompras();
+    }
+  },
+};
 </script>
 
 <style scoped>
+.produtos-wrapper {
+  margin-bottom: 40px;
+}
+
+.vendedor span {
+  color: #e80;
+}
+
+.vendedor span {
+  margin-bottom: 20px;
+}
 </style>
--- src/views/usuario/UsuarioVendas.vue
+++ src/views/usuario/UsuarioVendas.vue
@@ -1,12 +1,92 @@
 <template>
-  <div>
-    <h1>Lista de Produtos</h1>
-  </div>
+  <section>
+    <div v-if="vendas">
+      <h2>Compras</h2>
+      <div
+        class="produtos-wrapper"
+        v-for="(venda, index) in vendas"
+        :key="index"
+      >
+        <ProdutoItem v-if="venda.produto" :produto="venda.produto">
+          <p class="vendedor">
+            <span>Comprador: </span>
+            {{ venda.vendedor_id }}
+          </p>
+        </ProdutoItem>
+        <div class="entrega">
+          <h3>Entrega:</h3>
+          <ul v-if="venda.endereco">
+            <li v-for="(value, key) in venda.endereco" :key="key">
+              {{ key }}: {{ value }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+import ProdutoItem from "@/components/ProdutoItem.vue";
+import { api } from "@/services.js";
+import { mapState } from "vuex";
+
+export default {
+  data() {
+    return {
+      vendas: null,
+    };
+  },
+  components: {
+    ProdutoItem,
+  },
+  computed: {
+    ...mapState(["usuario", "login"]),
+  },
+  methods: {
+    getVendas() {
+      api.get(`/transacao?vendedor_id=${this.usuario.id}`).then((res) => {
+        this.vendas = res.data;
+      });
+    },
+  },
+  watch: {
+    login() {
+      this.getVendas();
+    },
+  },
+  created() {
+    if (this.login) {
+      this.getVendas();
+    }
+  },
+};
 </script>
 
 <style scoped>
+.produto-wrapper {
+  margin-bottom: 40px;
+}
+
+.vendedor span {
+  color: #e80;
+}
+
+.vendedor span {
+  margin-bottom: 20px;
+}
+
+.entrega {
+  display: grid;
+  grid-template-columns: minmax(100px, 200px) 1fr;
+  gap: 20px;
+  margin-bottom: 60px;
+}
+h2 {
+  margin-bottom: 20px;
+}
+h3 {
+  margin: 0;
+  justify-self: end;
+}
 </style>
